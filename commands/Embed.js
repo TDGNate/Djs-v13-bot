799ede00f381f6e972b6355bd63d42a23c49fbed
100Pyrex <ThePyr33x@gmail.com>
2022-02-03 16:04:15 +0330

    Update Embed.js

--- commands/Embed.js
+++ commands/Embed.js
@@ -10,14 +10,14 @@ module.exports = {
     async execute(interaction) {
 				// Info / Embed
 				const embed = new MessageEmbed()
-        .setColor('#0053ff')
-        .setTitle(' B In Qesmat Migan ( Title ) ')
+        .setColor('#FFFFFF')
+        .setTitle(' This s ( title ) ')
         .setURL('https://github.com/Pyr33x/Djs-v13-bot')
-        .setAuthor( ' B Inja Ham Author Migan  ',' https://cdn.discordapp.com/attachments/874992162549420132/920559650389237791/8576-raccoon.png ')
-        .setDescription(' Inja Ham K Description e ', false)
+        .setAuthor( ' This s Author   ',' https://cdn.discordapp.com/attachments/874992162549420132/920559650389237791/8576-raccoon.png ')
+        .setDescription(' It`s Description ', false)
         .setThumbnail('https://cdn.discordapp.com/attachments/874992162549420132/920559650389237791/8576-raccoon.png')
-        .addField(' Injori Ham Mitunid Field Add Konid  ')
-        .setFooter('In Ham K Footer e ', 'https://cdn.discordapp.com/attachments/874992162549420132/920559650389237791/8576-raccoon.png');
+        .addField(' You can add field`s with the following code ')
+        .setFooter(' It`s footer ', 'https://cdn.discordapp.com/attachments/874992162549420132/920559650389237791/8576-raccoon.png');
 
 
 
